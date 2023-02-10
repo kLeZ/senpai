ff96be4f86ddb8c2d2fe7b636563441d3084928e
GitHub Action <action@github.com>
2023-02-10 16:20:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/klez/senpai/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/klez/senpai/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.klez.senpai.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.klez.senpai.MyBundle
 
 class MyApplicationService {
 
